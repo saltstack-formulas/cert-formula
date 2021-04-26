12030ab74a1bdf013db78960796a920f4487a104
Imran Iqbal <iqbalmy@hotmail.com>
2021-04-26 16:06:09 +0100

    test(rubocop): fix all violations

--- test/integration/default/controls/managed.rb
+++ test/integration/default/controls/managed.rb
@@ -1,4 +1,6 @@
-control 'managed' do 
+# frozen_string_literal: true
+
+control 'managed' do
   impact 1.0
   title 'certficates and keys are managed'
   desc 'verifies that certificates are present or absent'
@@ -13,48 +15,48 @@ control 'managed' do
     key_dir = '/etc/pki/tls/private/'
   end
 
-  describe file(cert_dir + 'cert.from.src.to.add.crt') do
-    its('content') { should match /^-----BEGIN CERTIFICATE-----/ }
-    its('content') { should match /^CERT FROM SOURCE FILE/ }
-    its('content') { should match /^-----END CERTIFICATE-----/ }
+  describe file("#{cert_dir}cert.from.src.to.add.crt") do
+    its('content') { should match(/^-----BEGIN CERTIFICATE-----/) }
+    its('content') { should match(/^CERT FROM SOURCE FILE/) }
+    its('content') { should match(/^-----END CERTIFICATE-----/) }
   end
-  describe file(cert_dir + 'cert.and.key.to.add.crt') do
-    its('content') { should match /^-----BEGIN CERTIFICATE-----/ }
-    its('content') { should match /^1MOCKED CERT AND KEY/ }
-    its('content') { should match /^-----END CERTIFICATE-----/ }
+  describe file("#{cert_dir}cert.and.key.to.add.crt") do
+    its('content') { should match(/^-----BEGIN CERTIFICATE-----/) }
+    its('content') { should match(/^1MOCKED CERT AND KEY/) }
+    its('content') { should match(/^-----END CERTIFICATE-----/) }
   end
-  describe file(key_dir + 'cert.and.key.to.add.key') do
-    its('content') { should match /^-----BEGIN RSA PRIVATE KEY-----/ }
-    its('content') { should match /^1MOCKED CERT AND KEY/ }
-    its('content') { should match /^-----END RSA PRIVATE KEY-----/ }
+  describe file("#{key_dir}cert.and.key.to.add.key") do
+    its('content') { should match(/^-----BEGIN RSA PRIVATE KEY-----/) }
+    its('content') { should match(/^1MOCKED CERT AND KEY/) }
+    its('content') { should match(/^-----END RSA PRIVATE KEY-----/) }
   end
-  describe file(cert_dir + 'cert.and.key.with.ext.to.add.pem') do
-    its('content') { should match /^-----BEGIN CERTIFICATE-----/ }
-    its('content') { should match /^4MOCKED CERT AND KEY/ }
-    its('content') { should match /^-----END CERTIFICATE-----/ }
+  describe file("#{cert_dir}cert.and.key.with.ext.to.add.pem") do
+    its('content') { should match(/^-----BEGIN CERTIFICATE-----/) }
+    its('content') { should match(/^4MOCKED CERT AND KEY/) }
+    its('content') { should match(/^-----END CERTIFICATE-----/) }
   end
-  describe file(key_dir + 'cert.and.key.with.ext.to.add.priv') do
-    its('content') { should match /^-----BEGIN RSA PRIVATE KEY-----/ }
-    its('content') { should match /^4MOCKED CERT AND KEY/ }
-    its('content') { should match /^-----END RSA PRIVATE KEY-----/ }
+  describe file("#{key_dir}cert.and.key.with.ext.to.add.priv") do
+    its('content') { should match(/^-----BEGIN RSA PRIVATE KEY-----/) }
+    its('content') { should match(/^4MOCKED CERT AND KEY/) }
+    its('content') { should match(/^-----END RSA PRIVATE KEY-----/) }
   end
-  describe file(cert_dir + 'cert.to.add.crt') do
-    its('content') { should match /^-----BEGIN CERTIFICATE-----/ }
-    its('content') { should match /^2MOCKED CERT/ }
-    its('content') { should match /^-----END CERTIFICATE-----/ }
+  describe file("#{cert_dir}cert.to.add.crt") do
+    its('content') { should match(/^-----BEGIN CERTIFICATE-----/) }
+    its('content') { should match(/^2MOCKED CERT/) }
+    its('content') { should match(/^-----END CERTIFICATE-----/) }
   end
-  describe file(cert_dir + 'cert.and.key.to.remove.crt') do
+  describe file("#{cert_dir}cert.and.key.to.remove.crt") do
     it { should_not exist }
   end
-  describe file(key_dir + 'cert.and.key.to.remove.key') do
+  describe file("#{key_dir}cert.and.key.to.remove.key") do
     it { should_not exist }
   end
   if os.family == 'debian'
     describe file('/etc/ssl/certs/ca-certificates.crt') do
-      its('content') { should match /^CERT FROM SOURCE FILE/ }
-      its('content') { should match /^1MOCKED CERT AND KEY/ }
-      its('content') { should match /^2MOCKED CERT/ }
-      its('content') { should_not match /^3MOCKED CERT AND KEY/ }
+      its('content') { should match(/^CERT FROM SOURCE FILE/) }
+      its('content') { should match(/^1MOCKED CERT AND KEY/) }
+      its('content') { should match(/^2MOCKED CERT/) }
+      its('content') { should_not match(/^3MOCKED CERT AND KEY/) }
     end
   end
 end
--- test/integration/default/controls/nocerts.rb
+++ test/integration/default/controls/nocerts.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 control 'nocerts' do
   impact 1.0
   title 'no certificates or keys are managed'
-  desc 'verifies that if you do not specify certificates or keys, this formula will not cause problems'
+  desc 'verifies that if you do not specify certificates or keys, ' \
+       'this formula will not cause problems'
+
   if os.family == 'debian'
     describe file('/etc/ssl/certs/ca-certificates.crt') do
       it { should exist }
